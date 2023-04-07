44f439f278c2fdb966de21c514b31381cab8b793
Quang Minh <zemorts@gmail.com>
2023-04-07 03:33:57 +0000

    amlich

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask, Response
-import gettracnghiem
 from threading import Thread
 import json
+from flask import Flask, Response, request
 from flask_cors import CORS
+import gettracnghiem
+import lunar
 
 app = Flask(__name__)
 cors = CORS(app)
-from duckduckgo_search import ddg
 
 @app.route('/')
 def home():
@@ -18,5 +18,13 @@ def tracnghiem(q):
     json_string = json.dumps(output, ensure_ascii=False)
     return Response(json_string, mimetype='application/json')
 
+@app.route('/lunar', methods=['GET'])
+def lunar_convert():
+    date = request.args.get('date')
+    print('hellooooo')
+    lular_date = lunar.convert(date)
+    return Response(lular_date, mimetype='application/json')
+
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0',port=8080)
+    app.run(debug=False)
+    # app.run(debug=True,host='0.0.0.0',port=8080)

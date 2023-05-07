ee6ce0657e3ace57504cbbe429a02e110e5c5175
stromez <zemorts@gmail.com>
2023-05-08 00:14:25 +0700

    ?

--- modules/logger.py
+++ modules/logger.py
@@ -5,5 +5,5 @@ def write(data, filename='log.txt'):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     log_line = '{} - {}\n'.format(timestamp, data)
     print(data)
-    with open(f'./tmp/files/{filename}', 'a') as f:
+    with open(f'/tmp/files/{filename}', 'a') as f:
         f.write(log_line)

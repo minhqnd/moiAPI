eeb4098ee400e5f3144447dd61159ab5b4d3241b
stromez <zemorts@gmail.com>
2023-05-09 22:16:49 +0700

    da luong

--- modules/splitbeat.py
+++ modules/splitbeat.py
@@ -2,6 +2,7 @@ import os
 import requests
 import time
 from pydub import AudioSegment
+import concurrent.futures
 
 LALALAI_UPLOAD_URL = 'https://www.lalal.ai/api/upload/'
 LALALAI_CHECK_URL = "https://www.lalal.ai/api/check/"
@@ -10,34 +11,35 @@ LALALAI_PREWVIEW_URL = "https://www.lalal.ai/api/preview/"
 
 
 def split_and_merge_music(file_path):
-    # name_file = file_path.split('/')[-1]
     filename = os.path.basename(file_path)
     name_file = os.path.splitext(filename)[0]
-    # Tách file nhạc thành các đoạn 1 phút
     music_segments = split_music(file_path)
     if not music_segments:
         return {'status': 'error', 'message': 'Failed to split music.'}
 
-    # Gửi từng đoạn lên API để tách beat và vocal
-    print(f'Có tổng {len(music_segments)} files')
     result_segments = []
-    for segment in music_segments:
+
+    def process_segment(segment):
         segment_id = upload_music_segment(segment)
         if segment_id:
-            print('Upload xong')
             post_preview(segment_id)
             result = wait_for_segment_processing(segment_id)
             if result:
-                result_segments.append(result)
-                # print(result_segments)
+                return result
 
-    # Tổng hợp kết quả và ghép lại các file stem_track và back_track
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Sử dụng map để gửi các yêu cầu bất đồng bộ và thu thập kết quả theo đúng thứ tự
+        results = executor.map(process_segment, music_segments)
+
+        for result in results:
+            if result:
+                result_segments.append(result)
+
     merged_stem_track_path, merged_back_track_path = merge_music_segments(
         result_segments, name_file)
     if not merged_stem_track_path or not merged_back_track_path:
         return {'status': 'error', 'message': 'Failed to merge music segments.'}
 
-    # Trả về đường dẫn của các file stem_track và back_track đã ghép
     return {'status': 'success', 'stem_track': merged_stem_track_path, 'back_track': merged_back_track_path}
 
 
@@ -93,9 +95,6 @@ def upload_music_segment(segment):
         print(response.text)
 
 
-import time
-import requests
-
 def wait_for_segment_processing(segment_id):
     # Chờ xử lý và kiểm tra trạng thái của đoạn nhạc
     while True:
@@ -109,55 +108,49 @@ def wait_for_segment_processing(segment_id):
         # print(response.json())
         if response.status_code == 200:
             json_data = response.json()
-            segment_status = json_data['result'].get(segment_id, {}).get('preview', {}).get('stem_track')
+            segment_status = json_data['result'].get(
+                segment_id, {}).get('preview', {}).get('stem_track')
             if segment_status is not None:
                 return json_data['result']
             elif segment_status == 'error':
                 return None
 
 
-
-
 def merge_music_segments(result_segments, name_file):
-    # Tải về và ghép lại các file stem_track và back_track
     stem_tracks = []
     back_tracks = []
-    index = 0
-    for result_segment in result_segments:
-        segment_id = list(result_segment.keys())[0]
-        print(segment_id)
-        stem_track_url = result_segment[segment_id]['preview']['stem_track']
-        back_track_url = result_segment[segment_id]['preview']['back_track']
-        
-        stem_track_path = download_music_track(segment_id, stem_track_url)
-        back_track_path = download_music_track(segment_id, back_track_url)
 
-        index+=1
-        if stem_track_path and back_track_path:
-            stem_tracks.append(stem_track_path)
-            back_tracks.append(back_track_path)
+    def download_track(segment_id, track_url):
+        response = requests.get(track_url)
+        if response.status_code == 200:
+            track_path = f"./tmp/{segment_id}_{track_url.split('/')[-1]}.mp3"
 
+            with open(track_path, 'xb') as file:
+                file.write(response.content)
 
-    # Ghép lại các file stem_track và back_track
+            return track_path
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Sử dụng map để tải xuống các tệp cùng một lúc và thu thập kết quả theo thứ tự
+        track_urls = [(list(result_segment.keys())[0], result_segment[list(result_segment.keys())[
+                       0]]['preview']['stem_track']) for result_segment in result_segments]
+        stem_tracks = list(executor.map(
+            lambda args: download_track(*args), track_urls))
+
+        track_urls = [(list(result_segment.keys())[0], result_segment[list(result_segment.keys())[
+                       0]]['preview']['back_track']) for result_segment in result_segments]
+        back_tracks = list(executor.map(
+            lambda args: download_track(*args), track_urls))
+
+    # Loại bỏ các giá trị None (không tải được tệp) khỏi danh sách
+    stem_tracks = [track for track in stem_tracks if track is not None]
+    back_tracks = [track for track in back_tracks if track is not None]
+
     merged_stem_track_path = merge_tracks(stem_tracks, name_file + '_vocal')
     merged_back_track_path = merge_tracks(back_tracks, name_file + '_beat')
 
-    # Trả về đường dẫn của các file stem_track và back_track đã ghép
     print(merged_stem_track_path, merged_back_track_path)
     return merged_stem_track_path, merged_back_track_path
-
-
-def download_music_track(segment_id, track_url):
-    # Tải về file nhạc từ URL
-    
-    response = requests.get(track_url)
-    if response.status_code == 200:
-        track_path = f"./tmp/{segment_id}_{track_url.split('/')[-1]}.mp3"
-
-        with open(track_path, 'xb') as file:
-            file.write(response.content)
-        print(track_path)
-        return track_path
 
 
 def merge_tracks(tracks, name_file):
@@ -179,5 +172,6 @@ def merge_tracks(tracks, name_file):
 
 # upload_music_segment(AudioSegment.from_file('./test.mp3'))
 # print(upload_music_segment('./test.mp3'))
-print(split_and_merge_music('./test.mp3'))
 # wait_for_segment_processing('2473503a-7560-4ab6-8aba-929862acf47c')
+print(split_and_merge_music('./ccc.wav'))
+# print(split_and_merge_music('./test.mp3'))
